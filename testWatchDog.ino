a2d1947c645c3a9b651486aca5a588d74526c2d3
iroshanjaya <irojaya@gmail.com>
2015-01-24 08:52:31 +0530

    Update testWatchDog.ino

--- testWatchDog.ino
+++ testWatchDog.ino
@@ -1,25 +1,27 @@
 #include <avr/wdt.h>
 
-void setup () { Serial.begin (9600);
+void setup () { 
+ Serial.begin (9600);
+ 
+ Serial.println ("Restarted.");
 
-Serial.println (“Restarted.”);
-
-wdt_enable (WDTO_8S); // reset after four second, if no “pat the dog” received
+ wdt_enable (WDTO_4S); // reset after four second, if no “pat the dog” received
 
 } // end of setup
 
 void loop () {
 
-Serial.println (“Entered loop …”);
+  Serial.println ("Entered loop...");
 
-while (true) {
+     while (true) {
 
- wdt_reset (); 
- /*keep in the while loop. this clear watchdog timer. if delay()>WDT_xS, then MSU reset. or if MCU stuck
-   then WDT timer will reset MSU after WDTO_xS. because it will not reach wdt_reset() command.            
- */
+         wdt_reset (); 
+          //keep in the while loop. this clear watchdog timer. if delay()>WDT_xS, then MSU reset. or if MCU stuck
+          //then WDT timer will reset MSU after WDTO_xS. because it will not reach wdt_reset() command.            
+ 
  Serial.println ("Entered while ...");
- delay(5000);
+ 
+ delay(2000);
 
 }   // oops, went into a loop
 } // end of loop
